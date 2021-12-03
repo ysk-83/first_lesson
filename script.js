6ad3e1f490ef580536b5d7ac691fc9bf22721c2a
Yurii <ykulikk@gmail.com>
2021-12-03 14:46:09 +0200

    начало програмрования js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+ let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
  
+ let personalMovieDB = {
+     count: numberOfFilms,
+     movies: {},
+     actors: {},
+     genres: [],
+     privat: false,
+ };
+
+for(let i = 0; i < 2; i++) {
+    let film = prompt('Один из последних просмотренных фильмов?');
+    let rank = prompt('На сколько оцените его?');
+    personalMovieDB.movies[film] = +rank;
+    
+}
+
+console.log(personalMovieDB);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
